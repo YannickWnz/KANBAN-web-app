fe66c80f3a78f71da4c41a5b45e917fd1083c864
yannickwnz <yannickwnz@gmail.com>
2023-02-01 00:26:54 +0000

    file returning number of task in columns

---
+++ include/fetchtodotaskscount.inc.php
@@ -0,0 +1,17 @@
+<?php
+include '../config/dbh.php';
+include '../classes/sanitize.class.php';
+include '../classes/Account.class.php';
+
+$insert = new Account($con);
+
+if(isset($_GET['bid'])) {
+    // $boardId = Sanitize::sanitizeId($_GET['bid']);
+    $boardId = $_GET['bid'];
+}
+
+echo $insert->fetchNumberOfTodoTask($boardId);
+
+// echo $boardID;
+
+
