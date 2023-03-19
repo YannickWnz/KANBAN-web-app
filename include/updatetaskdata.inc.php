ec12a0c5ae7fd83ea0a8f68b14f69134117a622b
yannickwnz <yannickwnz@gmail.com>
2023-03-19 13:09:55 +0000

    update task php

---
+++ include/updatetaskdata.inc.php
@@ -0,0 +1,45 @@
+<?php
+include '../config/dbh.php';
+include '../classes/sanitize.class.php';
+include '../classes/Account.class.php';
+
+$insert = new Account($con);
+
+// check 
+
+// $user = $_SESSION['visited'];
+
+// get data from js file
+$jsonReqUrl = "php://input";
+$reqjson = file_get_contents($jsonReqUrl);
+$data = json_decode($reqjson, true);
+
+$taskTitle = Sanitize::sanitizeInput($data['edit_task_title']);
+$taskDescription = Sanitize::sanitizeDescription($data['edit_task_description']);
+$substask1 = Sanitize::sanitizeInput($data['edit_task_subtask1']);
+$substask2 = Sanitize::sanitizeInput($data['edit_task_subtask2']);
+$substask3 = Sanitize::sanitizeInput($data['edit_task_subtask3']);
+$taskID = Sanitize::sanitizeId($data['selected_task_id']);
+$taskStatus = Sanitize::sanitizeStatus($data['edit_task_status']);
+
+
+if(strlen($substask2) == 0 && strlen($substask3) == 0){
+    $sub = array("$substask1");
+    $completedSub = array("false");
+} else if (strlen($substask3) == 0) {
+    $sub = array("$substask1", "$substask2");
+    $completedSub = array("false", "false");
+} else {
+    $sub = array("$substask1", "$substask2", "$substask3");
+    $completedSub = array("false", "false", "false");
+}
+
+
+// turn array into json format
+$subtasks = json_encode($sub);
+$completeSubtask = json_encode($completedSub);
+// echo $subtasks . ' ' . $completeSubtask;
+
+$insert->updateTaskData($taskID, $taskTitle, $taskDescription, $subtasks, $completeSubtask, $taskStatus);
+
+
