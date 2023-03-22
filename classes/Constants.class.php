b319853cfbc1b529defe48205b41b8b973494690
yannickwnz <yannickwnz@gmail.com>
2023-03-22 14:31:53 +0000

    constant class file

---
+++ classes/Constants.class.php
@@ -0,0 +1,14 @@
+<?php
+
+class Constants {
+
+    public static $usernameTaken = 'Username already taken';
+    public static $pwdValidation = 'Password should be at least 8 characters in length and should include at least one upper case letter, one number, and one special character.';
+    public static $emptyInput = 'Please fill in all fields';
+    public static $emptyLoginInputs = 'Please fill in all fields';
+    public static $pwdMatch = 'Passwords do not match';
+    public static $pwdCharLength = 'Password should be at least 6 characters long';
+    public static $pwdTaken = 'This password is already taken. Please choose a different password';
+    public static $error = 'An error occurred. Please try again';
+    public static $invalidUser = 'Invalid usename or password';
+}
